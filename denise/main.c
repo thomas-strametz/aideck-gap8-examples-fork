c315b15b9a3ef1fcfe809be6d59a97d5fbc1ba66
Thomas Strametz <thomas.strametz2@fh-joanneum.at>
2025-03-04 14:15:25 +0100

    Update

--- denise/main.c
+++ denise/main.c
@@ -5,8 +5,11 @@
 void start_example(void) {
     pi_bsp_init();
     cpxInit();
+
+    int counter = 0;
     while (1) {
-        cpxPrintToConsole(LOG_TO_CRTP, "Hello Denise!\n");
+        cpxPrintToConsole(LOG_TO_CRTP, "Hello Denise %d!\n", counter);
+        counter++;
         pi_time_wait_us(1000*1000);
     }
 }
