b48c019a20128f6419844ce7dcbf8ea0e190088f
Thomas Strametz <thomas.strametz2@fh-joanneum.at>
2025-04-09 10:47:04 +0200

    Improvements

--- examples/ai/classification/merge_datasets.py
+++ examples/ai/classification/merge_datasets.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 from pathlib import Path
 
 
@@ -6,6 +7,7 @@ def get_options():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--src-ds', type=str, nargs='+', default=[''])
     arg_parser.add_argument('--dst-ds', type=str, default='dst_dataset')
+    arg_parser.add_argument('--training-structure', action='store_true', default=False)
     return arg_parser.parse_args()
 
 
@@ -26,11 +28,41 @@ def copy_to_dst(dst_ds, src_ds, sub_folder):
                 dst_file.write(src_file.read())
 
 
+def copy_files(dst_ds, files):
+    dst_folder = Path(dst_ds)
+    dst_folder.mkdir(exist_ok=True, parents=True)
+
+    for file in files:
+        with open(dst_folder.joinpath(file.name), 'wb') as dst_file:
+            with open(file, 'rb') as src_file:
+                dst_file.write(src_file.read())
+
+
 def main():
     opt = get_options()
     dst_ds = Path(opt.dst_ds)
-    copy_to_dst(dst_ds, opt.src_ds, 'patch')
-    copy_to_dst(dst_ds, opt.src_ds, 'no_patch')
+
+    if opt.training_structure:
+        patch_images = list(src_img_gen(opt.src_ds, 'patch'))
+        random.shuffle(patch_images)
+        patch_train_size = int(len(patch_images) * 0.8)
+        train_patch_images = patch_images[:patch_train_size]
+        val_patch_images = patch_images[patch_train_size:]
+
+        copy_files(dst_ds.joinpath('train/patch'), train_patch_images)
+        copy_files(dst_ds.joinpath('validation/patch'), val_patch_images)
+
+        no_patch_images = list(src_img_gen(opt.src_ds, 'no_patch'))
+        random.shuffle(no_patch_images)
+        no_patch_train_size = int(len(no_patch_images) * 0.8)
+        train_no_patch_images = no_patch_images[:no_patch_train_size]
+        val_no_patch_images = no_patch_images[no_patch_train_size:]
+
+        copy_files(dst_ds.joinpath('train/no_patch'), train_no_patch_images)
+        copy_files(dst_ds.joinpath('validation/no_patch'), val_no_patch_images)
+    else:
+        copy_to_dst(dst_ds, opt.src_ds, 'patch')
+        copy_to_dst(dst_ds, opt.src_ds, 'no_patch')
 
 
 if __name__ == '__main__':
